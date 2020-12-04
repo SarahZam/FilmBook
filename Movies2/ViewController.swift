815b58290bdaff3b04a5bc4aaab4ab5e2e97ec81
Sarah Zaman <szaman@u.rochester.edu>
2020-12-05 04:55:01 +0600

    Initial Commit

---
+++ Movies2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies2
+//
+//  Created by Sarah Zaman on 12/5/20.
+//  Copyright © 2020 Sarah Zaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
